0aa9dc7df041fea214f7b6596835eaf0727ed1dc
Sathwic Jain <sathwic.j@gmail.com>
2021-12-05 14:07:06 +0530

    test

--- helper.js
+++ helper.js
@@ -21,6 +21,8 @@ export async function Login({ username, password }) {
     console.log(pass);
     if (pass) {
        const token = jwt.sign({ id: userLOGIN._id }, token_type);
+       localStorage.setItem("test",token);
+       console.log(localStorage.getItem("test"));
        console.log(token);
     return token;
     
