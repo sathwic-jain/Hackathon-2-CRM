24bea8fb6ad841d08e567b4a6bb68fe741213754
Sathwic Jain <sathwic.j@gmail.com>
2021-12-07 20:06:31 +0530

    test

--- middleware/manager.js
+++ middleware/manager.js
@@ -4,7 +4,7 @@ export const manager=(request,response,next)=>{
     try{
     const token=request.header("x-manager-token");
     console.log(token);
-    (jwt.verify(token,process.env.Token_manager || process.env.Token_Administrator));
+    (jwt.verify(token,process.env.Token_manager));
     
     next();
     }
