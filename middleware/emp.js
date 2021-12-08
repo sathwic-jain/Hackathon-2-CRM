e0f6df1ce536f79d83c44e91f969582407abc58d
Sathwic Jain <sathwic.j@gmail.com>
2021-12-08 20:00:43 +0530

    test

--- middleware/emp.js
+++ middleware/emp.js
@@ -4,9 +4,7 @@ export const emp=(request,response,next)=>{
     try{
     const token=request.header("x-emp-token");
     console.log(token);
-    jwt.verify(token,process.env.Token_employee||
-    token,process.env.Token_manager ||
-    token,process.env.Token_admin);
+    jwt.verify(token,process.env.Token_Employee)
     next();
     }
     catch{
